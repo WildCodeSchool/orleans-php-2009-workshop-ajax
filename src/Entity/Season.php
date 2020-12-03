7e402fff92fcc557ca50c082ec449feeae000ae9
Matthieu Martinot <martinot.matthieu@gmail.com>
2020-12-03 11:25:43 +0100

    quest_12

--- src/Entity/Season.php
+++ src/Entity/Season.php
@@ -50,6 +50,11 @@ class Season
         $this->episodes = new ArrayCollection();
     }
 
+    public function __toString()
+    {
+        return $this->program->getTitle() . " : Saison " . $this->number;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
